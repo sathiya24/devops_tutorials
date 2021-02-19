def4b879c5861a10a9da6dc448246252a382b413
sathiya24 <ssnsathi125@gmail.com>
2021-02-19 13:30:47 +0530

    new file

---
+++ task2.sh
@@ -0,0 +1,25 @@
+echo #!/bin/bash
+
+src1=$1
+dst1=$2
+
+SAME=0
+x=`diff $1 $2`
+if [ "$x" != "" ]
+then 
+echo "already exists"
+
+if [ "$x" == "" ]
+SAME=1
+rsync -avu "$1/" "$2"
+rsync -avu "$2/" "$1"
+echo "copied"
+else
+echo "no difference"
+echo "$SAME"
+fils
+
+
+
+
+
