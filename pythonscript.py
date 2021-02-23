33bbb4e12d144f07efae856091f13e25ea389f13
sathiya24 <ssnsathi125@gmail.com>
2021-02-23 13:52:02 +0530

    pythonfile

---
+++ pythonscript.py
@@ -0,0 +1,9 @@
+import urllib.request, json
+with urllib.request.urlopen("https://jsonplaceholder.typicode.com/photos") as url:
+    data = json.loads(url.read().decode())
+for item in data:
+    url = item['url']
+    thumbnailUrl = item['thumbnailUrl']
+    urllib.request.urlretrieve(url, '/home/sathya/Downloads/python')
+    urllib.request.urlretrieve(thumbnailUrl, '/home/sathya/Downloads/python')
+
