b4f6889642280d538750decb1770e7b208c275c8
sathiya24 <76838150+sathiya24@users.noreply.github.com>
2021-03-05 16:58:57 +0530

    Update pythonscript.py

--- pythonscript.py
+++ pythonscript.py
@@ -5,7 +5,6 @@ data = json.loads(url.read().decode())
 for item in data:
     url = item['url']
     thumbnailUrl = item['thumbnailUrl']
-    os.listdir()
     urllib.request.urlretrieve(url, "/tmp/")
     urllib.request.urlretrieve(thumbnailUrl, "/tmp/")
 
