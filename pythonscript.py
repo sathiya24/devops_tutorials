055cbe06f4fe59cecd31767365b411b7a825b911
sathiya24 <76838150+sathiya24@users.noreply.github.com>
2021-03-05 14:50:35 +0530

    Update pythonscript.py

--- pythonscript.py
+++ pythonscript.py
@@ -1,4 +1,5 @@
-import urllib.request, json
+import json
+from six.moves import urllib
 with urllib.request.urlopen("https://jsonplaceholder.typicode.com/photos") as url:
     data = json.loads(url.read().decode())
 for item in data:
